1a72c7f921756bf2decf0800a2a2afabac6c7869
Yurio Windiatmoko <yuriowindiatmoko@gmail.com>
2022-05-28 08:53:44 +0000

    fix(main): tidying up file structure

--- executor/executor.py
+++ executor/executor.py
@@ -1,8 +1,8 @@
 from jina import Executor, DocumentArray, requests
-
+from .helper import print_something
 
 class MyExecutor98(Executor):
     """"""
     @requests
     def foo(self, docs: DocumentArray, **kwargs):
-        pass
+        print_something()
